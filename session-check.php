0e4fe9471a11cf53055866f26f402b3c2d068550
superdeeid <github@paquette.me>
2023-03-24 13:23:36 -0400

    Update session-check.php

--- session-check.php
+++ session-check.php
@@ -1,12 +1,7 @@
 <?php
-    //session_start();
     
     if ( !isset( $_COOKIE["LastVisitedSection"] ) ) {
         echo '1';
-        
-        //        unset($_SESSION['name']);
-        //        session_unset();
-        //        session_destroy();
     }
     else {
         echo '0';
